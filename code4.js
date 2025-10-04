2fc78b2a1ab1ca2672c705fcebd06afdfc975acb
unknown <gdutt2304@gmail.com>
2025-10-04 17:54:03 +0530

    Pipe

--- code4.js
+++ code4.js
@@ -2,7 +2,17 @@
 // console.log(buffer);
 // console.log(buffer.toString());
 
-// const fs = require("fs");
+const fs = require("fs");
+
+const readStream = fs.createReadStream("example2.txt");
+const writeStream = fs.createWriteStream("copyExample2.txt");
+readStream.pipe(writeStream);
+console.log("file copying started... ");
+
+writeStream.on("finish", () => {
+  console.log("file copying completed successfully");
+});
+// console.log(readStream);
 
 // const writeStream = fs.createWriteStream("example2.txt");
 // writeStream.write("Hello Node.js \n");
