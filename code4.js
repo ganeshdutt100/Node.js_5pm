3c37298cba03694005620c3db137b979a987ac6a
unknown <gdutt2304@gmail.com>
2025-10-04 17:36:32 +0530

    Stream

---
+++ code4.js
@@ -0,0 +1,25 @@
+// const buffer = Buffer.from("Hello world");
+// console.log(buffer);
+// console.log(buffer.toString());
+
+// const fs = require("fs");
+
+// const writeStream = fs.createWriteStream("example2.txt");
+// writeStream.write("Hello Node.js \n");
+// writeStream.write("Hello World");
+// writeStream.end();
+
+// writeStream.on("finish", () => console.log("file writing completed"));
+// writeStream.on("error", (err) => console.log(err));
+
+// const readStream = fs.createReadStream("example.txt", "utf8");
+// readStream.on("data", (chunks) => {
+//   console.log("New Chunk Received");
+//   console.log(chunks);
+// });
+
+// readStream.on("end", () => {
+//   console.log("file reading completed");
+// });
+
+// readStream.on("error", (err) => console.log(err));
