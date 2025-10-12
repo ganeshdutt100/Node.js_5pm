0e7c408faf955e4227493be04faf1ed44bb51aa8
unknown <gdutt2304@gmail.com>
2025-10-12 14:01:07 +0530

    Nodemon

--- code1/server.js
+++ code1/server.js
@@ -1,16 +1,17 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  // res.writeHead(200,{"Content-Type":"text/plain"})
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Node.js</title></head>");
-  res.write(`<body>
-    <h1>Node.js</h1>
-     <p>Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.</p>
-    </body>`);
-
-  res.write("</html>");
-  res.end();
+  const filePath = path.join(__dirname, "index.html");
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "content-Type": "text/plain" });
+      res.end("Error loading html file...");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
 });
 const PORT = 3000;
 server.listen(PORT, () => {
