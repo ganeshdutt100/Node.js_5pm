f684f1f6601f3484137667619cc51278ac7a177a
unknown <gdutt2304@gmail.com>
2025-11-11 13:36:04 +0530

    Update EJS and PUG

--- Express/code4.js
+++ Express/code4.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+// app.set("view engine", "ejs");
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
   res.send("i am Front page ");
 });
-app.get("/home", (req, res) => {
-  //   res.send("i am home page ");
-  res.sendFile(__dirname + "/public/index.html");
+// app.get("/home", (req, res) => {
+//   //   res.send("i am home page ");
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+
+app.get("/profile", (req, res) => {
+  const userProfile = {
+    name: "Yogendra",
+    age: 20,
+    jobTitle: "Frontend Developer",
+  };
+  res.render("index", { userProfile });
 });
 
 app.listen(3000, () => {
